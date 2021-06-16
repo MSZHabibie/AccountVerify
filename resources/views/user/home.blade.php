87ca6eefc765b27be0b27455d13d2cc07ce3f4a1
Muhammad Syah Zichrullah Habibie <mszhabibie@gmail.com>
2021-06-17 04:56:59 +0700

    Perubahan Oleh Habibie

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,22 +1,40 @@
 @extends('layouts/navbar')
-@section('title', 'Accify | Home')
+@section('title', 'Accify | Search')
 
 @section('body')
 
-<div class="page-section">
-            <div class="container" style="margin-top: 3rem;">
-                <h2>Temukan akun-akun resmi disini!</h2>
-                <div class="headerrr">
-    
+<div class="page-hero-section bg-image hero-home-1" style="background-image: url(../assets/img/bg4.jpeg); background-repeat: no-repeat; background-size: 100%; height:90%">
+    <div class="text-desc">
+        <h1 class="mb-4">Ayo cek akun resmi disini!</h1>
+        <p class="mb-4">Temukan lebih banyak nama-nama akun resmi <br>
+            dari sumber terpercaya untuk menghindari hal-hal <br>
+            yang tidak diinginkan </p> <br>
+        <div class="scroll"> scroll me now! </div>
+    </div>
+
+
+</div>
+
+<div class="page-hero-section bg-image hero-home-1" style="background-image: url(../assets/img/bg6.png); background-repeat: no-repeat; background-size: 100%;">
+
+    <div class="page-section">
+        <div class="container">
+            <div class="headerr">
+                <!-- <h1>Find what you want here</h1> -->
+                <form action="/cari" method="get">
                     <div class="form-box">
-                        <input type="text" class="search-field searching" placeholder="Cari akun">
-                        <button class="search-btn" type="button">Search </button>
-
+                        <input type="text" class="search-field searching" name="cari" placeholder="Cari akun" value="{{old('cari')}}">
+                        <button class="search-btn" type="submit" value="cari">Search </button>
                     </div>
-                </div>
-
+                </form>
             </div>
 
+            <div class="img-place ">
+                <img src="../assets/img/app_preview_2.png" alt="">
+            </div>
         </div>
 
+    </div>
+
+</div>
 @endsection
--- resources/views/user/profil.blade.php
+++ resources/views/user/profil.blade.php
@@ -3,77 +3,72 @@
 
 @section('body')
 
-<div class="col-15"  >
-<div class="card-body" style=" padding-top:2rem; ">
-    <div class="row" >
-        <div class="container  col-md-3" style="margin-left: 200px;" >
-            <img class="profil-img polaroid" src="../gambar/profil.png" alt="" width="400">     
-            <div class="row1">
-                <button type="" class="btn btn-primary btn-lg" href="/daftar" >Tambah Akun</button>
+<div class="col-15">
+    <div class="card-body" style=" padding-top:2rem; ">
+        <div class="row">
+            <div class="container  col-md-3" style="margin-left: 200px;">
+                <img class="profil-img polaroid" src="../gambar/profil.png" alt="" width="400">
+                <div class="row1">
+                    <a href="{{ url('user/daftar')}}" class="btn btn-primary btn-lg">Tambah Akun</a>
+                </div>
+
+                <br>
             </div>
-            <div class="row2">
-                <button type="submit" class="btn btn-success btn-lg">Edit</button>
-            </div>
-            <div class="row3">
-                <button type="submit" class="btn btn-danger btn-lg">Hapus</button>
-            </div>
-                 
-            
-            <br>
-        </div>
-        <div class="container rounded-3 col-md-5 ">
-            <div class="row">
+            <div class="container rounded-3 col-md-5 ">
+                <div class="row">
                     <div class="col-sm-3">
                         <h6 class="mb-2">First Name </h6>
                     </div>
                     <div class="col-sm-8 text-dark ">
-                        <h6 class="ctb"> {{ Auth::guard('web')->user()->first_name }} </h6> 
+                        <h6 class="ctb"> {{ Auth::guard('web')->user()->first_name }} </h6>
                     </div>
-                 
-            </div>
-            <br>
-            <div class="row">
+
+                </div>
+                <br>
+                <div class="row">
                     <div class="col-sm-3">
                         <h6 class="mb-2">Last Name </h6>
                     </div>
                     <div class="col-sm-8 text-dark ">
-                        <h6 class="ctb"> {{ Auth::guard('web')->user()->last_name }} </h6> 
+                        <h6 class="ctb"> {{ Auth::guard('web')->user()->last_name }} </h6>
                     </div>
-                  
-            </div>
-            <br>
-            <div class="row">
+
+                </div>
+                <br>
+                <div class="row">
                     <div class="col-sm-3">
                         <h6 class="mb-2">Email </h6>
                     </div>
                     <div class="col-sm-8 text-dark ">
-                        <h6 class="ctb"> {{ Auth::guard('web')->user()->email }} </h6> 
+                        <h6 class="ctb"> {{ Auth::guard('web')->user()->email }} </h6>
                     </div>
-                 
+
+                </div>
+
+                <div class="table-warga col-md-11">
+                    <br><br><br>
+                    <h5>List Akun</h5>
+                    <table class="table table-sm border-dark table-light" style="margin-top: 1em; font-size:1.3em">
+                        <thead class="table-dark">
+                            <tr>
+
+                                <th scope="col">Nama Akun</th>
+                                <th scope="col">Type</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($account as $acc)
+                            <tr>
+                                <td><a href="https://www.{{$acc->type}}.com/{{$acc->account}}"> {{$acc->account}} </a></td>
+                                <td><a href="https://www.{{$acc->type}}.com"> {{$acc->type}} </a></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
-
-        <div class="table-warga col-md-11" >
-            <br><br><br>
-        <h5>List Akun</h5>
-        <table class="table table-sm border-dark table-light" style="margin-top: 1em; font-size:1.3em">
-                    <thead class="table-dark">
-                    <tr>
-                       
-                        <th scope="col">Nama Akun</th>
-                        <th scope="col">Type</th>
-                    </tr>    
-                    </thead>
-                    <tbody>        
-                        <tr>
-                            <td>  a</td>
-                            <td>  b </td>           
-                        </tr>
-                    </tbody>
-                </table>
-            </div> 
-
         </div>
-    </div>
 
-</div>
-@endsection
+    </div>
+    @endsection
